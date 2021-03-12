c22671249c199525416d1301be5b02255a265c26
Luke McClure <lam3g16@soton.ac.uk>
2021-03-12 20:01:07 +0000

    Initial React submit page, landing page stubbed

---
+++ src/main/ui/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+import URLForm from '../../components/submission/submissionform'
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <URLForm />
+      </header>
+    </div>
+  );
+}
+
+export default Home;
